acfafaacb975f1154d4dc146b16d9bbc9ad4d4f5
aetana <amanuel.etanan@gmail.com>
2022-09-16 23:40:54 -0500

    project 5 part 2 my own top class

--- src/week2/lap5/prog2/Top.java
+++ src/week2/lap5/prog2/Top.java
@@ -4,45 +4,42 @@ public class Top {
 	int t = 1;
 	Middle mid;
 	Middle.Bottom midbot;
-
+	
 	public Top() {
 		mid = new Middle();
 		midbot = mid.new Bottom();
 	}
-
-	// returns the value in the instance vble of Bottom
+	//returns the value in the instance vble of Bottom
 	int readBottom() {
-		// implement
+		//implement
 		return midbot.b;
-
 	}
-
 	class Middle {
 		int m = 2;
-
-		// returns sum of instance vble in Top and
-		// instance vble in Bottom
+		//returns sum of instance vble in Top and 
+		//instance vble in Bottom
 		int addTopAndBottom() {
-			// implement
+			//implement
+
 			return t + midbot.b;
 		}
-
 		class Bottom {
 			int b = 3;
-
-			// returns the product of the instance vbles
-			// in all three classes
+			//returns the product of the instance vbles
+			//in all three classes
 			int multiplyAllThree() {
-				// implement
+				//implement
 				return t * m * b;
 			}
 		}
 	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Top top = new Top();
+
 		System.out.println(top.readBottom());
 		System.out.println(top.mid.addTopAndBottom());
 		System.out.println(top.midbot.multiplyAllThree());
+
 	}
+
 }
